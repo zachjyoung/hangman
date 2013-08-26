0c638a2fb65f072d712c858d63979467d18f85b6
Zach Young <zachjyoung@gmail.com>
2013-08-25 23:40:53 -0400

    pretty much there, doesn't reset or match completed word

--- hangman.rb
+++ hangman.rb
@@ -6,7 +6,7 @@ hidden_word =  "_" * random_word.length
 guessed_letters = []
 guess_count = 9
 
-play_game = 
+play_game = false
 
 puts "Yo dog, I heard you like Hangman.  Fancy a game? Answer (yes or no) "
 play_game = gets.chomp
@@ -16,7 +16,6 @@ if play_game.downcase == "no"
   exit 
 else
   while guessed_letters != random_word
-  #change this, not very clear when you're trying to play
     puts hidden_word
     puts "letters guessed so far #{guessed_letters.join(",")}"
   #rename guess count!
@@ -31,57 +30,28 @@ else
         puts "Oh no, the point is not to HANG the MAN.  The right answer was #{random_word.upcase}"
         break
       end
-  elsif player_guess.length == 1
-      guessed_letters << player_guess
-      if random_word.include?(player_guess)
-        puts "#{player_guess} was found in the word, good job"
-        guess_count -=1
+    elsif player_guess.length == 1
+      if guessed_letters.include?(player_guess)
+        puts "Ah, ah, ah, you already guessed that, guess again"
+        guess_count -= 1
       else
-        guess_count -=1
-        puts "Sorry no #{player_guess}'s, go fish."
+        guessed_letters << player_guess
+        if random_word.include?(player_guess)
+          puts "#{player_guess} was found in the word, good job"
+          guess_count -= 1
+        else
+          guess_count -= 1
+          puts "Sorry no #{player_guess}'s, go fish."
+        end
+      end
+      hidden_word = random_word.gsub(/[^"#{guessed_letters.join}"]/, "_")
+      if player_guess == hidden_word
+        puts "You did it, you guessed #{random_word.upcase} in #{guess_count} turns!"
+        break
+      elsif guess_count == 0
+        puts "Cool try bro, but #{random_word.upcase} was the word."
+        break
       end
     end
-    hidden_word = random_word.gsub(/[^"#{guessed_letters.join}"]/, "_")
-    if player_guess == hidden_word
-      puts "You did it, you guessed #{random_word.upcase} in #{guess_count} turns!"
-      break
-    elsif guess_count == 0
-      puts "Cool try bro, but #{random_word.upcase} was the word."
-      break
-    end
   end
-  puts "Would you like to play again?"
-  puts play_game.downcase
 end
-
-
-
- 
-
-# As a player
-# I want to know how much of the word I've guessed
-# So that I know how much remains   √
-
-# As a player
-# I want to see how many incorrect guesses I have remaining
-# So that I know how much I should be worried   √
-
-# As a gamemaster
-# I want to prompt the user for input
-# So that we can continue the game    √
-
-# As a player
-# I want to be able to guess a single letter or the whole word
-# So that I can make progress    √
-
-# As a gamemaster
-# I want to check the player's guessed letter against the word
-# So that I know which letters to unveil. 
-
-# As a gamemaster
-# I want to check the player's guessed word
-# So that I know if they won or not √
-
-# As a gamemaster
-# I want to end the game when the player has no more chances
-# So that the game remains challenging √
